dd0d25027c7956472c3ba98cbef8abc14137142c
dhruvpatil1215 <148572263+dhruvpatil1215@users.noreply.github.com>
2026-01-31 13:09:00 +0530

    Add files via upload

---
+++ backend/node-js/routes/location.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const locationController = require('../controllers/locationController');
+
+router.get('/countries', locationController.getCountries);
+router.get('/states/:country', locationController.getStates);
+router.get('/cities/:state', locationController.getCities);
+
+module.exports = router;
